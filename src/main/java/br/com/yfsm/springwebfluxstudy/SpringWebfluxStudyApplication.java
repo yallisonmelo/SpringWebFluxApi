8ce9d9e3d0a592a699639b855678f39c7444f813
yallisonmelo <yallisonmelo@gmail.com>
2024-03-07 15:26:13 -0300

    Create docker compose postgres

---
+++ src/main/java/br/com/yfsm/springwebfluxstudy/SpringWebfluxStudyApplication.java
@@ -0,0 +1,13 @@
+package br.com.yfsm.springwebfluxstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/yfsm/springwebfluxstudy/SpringWebfluxStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.yfsm.springwebfluxstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
